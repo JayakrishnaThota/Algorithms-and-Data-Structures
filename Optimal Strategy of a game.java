f8e490790128242ba55725c7e94a47f171f9af81
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2017-10-04 03:25:37 -0400

    Update Optimal Strategy of a game.java

--- Optimal Strategy of a game.java
+++ Optimal Strategy of a game.java
@@ -3,7 +3,7 @@ public int optimalstrategy(int[] array)
     int n = array.length;
     int[][] dp = new int[n][n];
     for(int i=0;i<n;i++){
-        for(int j=0,k=i;j<n;i++,j++){
+        for(int j=0,k=i;j<n;k++,j++){
             // (F(i+2,j)
             int a = (i+2<=j?dp[i+2][j]:0);
             // (F(i+1,j-1)
