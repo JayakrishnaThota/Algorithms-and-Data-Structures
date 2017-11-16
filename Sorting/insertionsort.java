6af28d4f8371d909dc048f23f1b6efd43850fb5a
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2017-11-15 23:45:03 -0500

    Create insertionsort.java

---
+++ Sorting/insertionsort.java
@@ -0,0 +1,33 @@
+package sorting;
+import java.util.Scanner;
+
+public class insertionsort {
+
+	public void insertionsort(int[] A){
+		int i=0,j=0;
+		for(i=1;i<A.length;i++){
+			int key = A[i];
+			j = i;
+			while(j>0&&A[j-1]>key){
+				A[j] = A[j-1];
+				j--;
+			}
+			A[j] = key;
+		}
+	}
+
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		System.out.println("Kindly enter the length of the array you want to sort");
+		int length = in.nextInt();
+		int[] array = new int[length];
+		System.out.println("Kindly enter the contents of the array");
+		for(int i=0;i<length;i++){
+			array[i] = in.nextInt();
+		}
+		insertionsort i = new insertionsort();
+		i.insertionsort(array);
+		for(int j=0;j<array.length;j++)
+			System.out.println(array[j]);
+	}
+}
