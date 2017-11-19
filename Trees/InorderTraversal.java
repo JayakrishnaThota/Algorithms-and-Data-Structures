2545474538be079d4996352399acd25857a9d0e6
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2017-11-19 01:33:24 -0500

    Update InorderTraversal.java

--- Trees/InorderTraversal.java
+++ Trees/InorderTraversal.java
@@ -1,1 +1,21 @@
+public void recInorder(TreeNode root){
+  if(root!=null){
+    recInorder(root.left);
+    System.out.println(root.val);
+    recInorder(root.right);
+  }
+}
 
+public List<Integer> Inorder(TreeNode root){
+  List<Integer> result = new ArrayList();
+  if(root==null) return result;
+  Stack<TreeNode> stack = new Stack();
+  stack.push(root);
+  while(!stack.isEmpty()){
+    TreeNode temp = stack.pop();
+    result.add(temp.val);
+    if(temp.right!=null) stack.push(temp.right);
+    if(temp.left!=null) stack.push(temp.left);
+  }
+  return result;
+}
