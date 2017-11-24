b77d045b8a84414d6d09c650286351a46c40b7e8
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2017-11-23 23:16:59 -0500

    Update PostorderTraversal.java

--- Trees/PostorderTraversal.java
+++ Trees/PostorderTraversal.java
@@ -8,28 +8,28 @@ public void recPostorder(TreeNode root){
 
 public List<Integer> Postorder(TreeNode root){
   List<Integer> result = new ArrayList();
-  if(root==null) return result;
-  Stack<TreeNode> stack = new Stack();
-  stack.push(root);
-  TreeNode prev = null;
-  while(!stack.isEmpty()){
-    //Check if traversing down
-    TreeNode cur = stack.peek();
-    if(prev==null || prev.left==cur || prev.right==cur){
-      if(cur.left!=null)
-        stack.push(cur.left);
-      else
-        stack.push(cur.right);
-    }
-    else if(cur.left==prev){
-      if(cur.right!=null)
-        stack.push(cur.right);
-    }
-    else{
-      result.add(cur.val);
-      stack.pop();
-    }
-    prev = cur;
-  }
-  return result;
+        if(root==null) return result;
+        Stack<TreeNode> stack = new Stack();
+        stack.push(root);
+        TreeNode prev = null;
+        while(!stack.isEmpty()){
+        //Check if traversing down
+            TreeNode cur = stack.peek();
+            if(prev==null || prev.left==cur || prev.right==cur){
+              if(cur.left!=null)
+                stack.push(cur.left);
+              else if(cur.right!=null)
+                stack.push(cur.right);
+            }
+            else if(cur.left==prev){
+              if(cur.right!=null)
+                stack.push(cur.right);
+            }
+            else{
+              result.add(cur.val);
+              stack.pop();
+            }
+            prev = cur;
+          }
+          return result;
 }
