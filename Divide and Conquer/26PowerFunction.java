a4da1fea21ba9f0c38acc4577633dff0a4789f08
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2018-01-16 13:19:30 -0500

    Update 26PowerFunction.java

--- Divide and Conquer/26PowerFunction.java
+++ Divide and Conquer/26PowerFunction.java
@@ -1,15 +1,10 @@
-//Calculate k power n
-public class Solution 
-{
-    public double power(int k, int n)
-	{
-       if(n==0) return 1;
-	   double half = power(k,n/2);
-	   if(n%2==0)
-	   return half*half;
-	   else if(n%2==1)
-	   return half*half*k;
-	   else
-	   return half*half/n;
+//O(logn) time and space
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0) return 1.0;
+        double half = myPow(x,n/2);
+        if(n%2==0) return half*half;
+        else if(n>0) return half*half*x;
+        else return half*half/x;
     }
 }
