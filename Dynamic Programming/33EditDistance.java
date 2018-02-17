40c7ab40f96bdee9130d0bb29e16ad82d94b7534
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2018-02-17 01:02:28 -0500

    Update 33EditDistance.java

--- Dynamic Programming/33EditDistance.java
+++ Dynamic Programming/33EditDistance.java
@@ -1,23 +1,35 @@
-public class Solution {
-    public int minDistance(String word1, String word2) 
-    {
-      int m = word1.length();
-      int n = word2.length();
-      int[][] dp = new int[m+1][n+1];
-        for(int i=0;i<=m;i++)
-             dp[i][0] = i;
-        for(int j=0;j<=n;j++)
-             dp[0][j] = j;
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(word1.charAt(i-1)==word2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = 1 + Math.min(Math.min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]);
+package DynamicProgramming;
+import java.util.*;
+public class EditDistance {
+    public int EditDistance(String a, String b) {
+        int m = a.length();
+        int n = b.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
                 }
             }
         }
         return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        EditDistance e = new EditDistance();
+        System.out.println("Please enter the two strings");
+        String a = in.nextLine();
+        String b = in.nextLine();
+        System.out.println("Edit distance between two strings a and b is" + e.EditDistance(a, b));
+        in.close();
     }
 }
