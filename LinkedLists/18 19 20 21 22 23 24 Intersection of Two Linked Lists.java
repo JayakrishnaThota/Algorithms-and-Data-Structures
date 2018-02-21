b99f3b17d6c37e7af544b4bb266bee7de4ebea95
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2018-02-21 03:55:13 -0500

    Create 18 19 20 21 22 23 24 Intersection of Two Linked Lists.java

---
+++ LinkedLists/18 19 20 21 22 23 24 Intersection of Two Linked Lists.java
@@ -0,0 +1,19 @@
+//O(n) and O(1)
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    //boundary check
+    if(headA == null || headB == null) return null;
+    
+    ListNode a = headA;
+    ListNode b = headB;
+    
+    //if a & b have different len, then we will stop the loop after second iteration
+    while( a != b){
+    	//for the end of first iteration, we just reset the pointer to the head of another linkedlist
+        a = a == null? headB : a.next;
+        b = b == null? headA : b.next;    
+    }
+    
+    return a;
+}
+}
