a6a901ecd25010de0775eb945f16fc857585f62b
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2018-02-24 16:45:49 -0500

    Create 16 17 18 Leaves, Full and half nodes.java

---
+++ 6 Trees/16 17 18 Leaves, Full and half nodes.java
@@ -0,0 +1,4 @@
+//Perform level order
+//If the nodes left and right child is null, increase leaf count by one
+//If the node has both left and right child, increase full count by one
+//If the nodes left child is null and right child is not null and viceversa, then increase half by one
