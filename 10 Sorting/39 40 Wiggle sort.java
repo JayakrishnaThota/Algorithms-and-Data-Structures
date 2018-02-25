7305326c231c4e6bba9a519ab92327e7a5ef1923
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2018-02-25 03:52:26 -0500

    Create 39 40 Wiggle sort.java

---
+++ 10 Sorting/39 40 Wiggle sort.java
@@ -0,0 +1,23 @@
+//O(n) and O(1)
+class Solution {
+   public void wiggleSort(int[] nums) {
+    boolean less = true;
+    for (int i = 0; i < nums.length - 1; i++) {
+        if (less) {
+            if (nums[i] > nums[i + 1]) {
+                swap(nums, i, i + 1);
+            }
+        } else {
+            if (nums[i] < nums[i + 1]) {
+                swap(nums, i, i + 1);
+            }
+        }
+        less = !less;
+    }
+}
+    public void swap(int[] A, int i, int j){
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+}
