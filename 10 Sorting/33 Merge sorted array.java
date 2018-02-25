f734e8add8c466282cba1381028c52bcc5d1de75
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2018-02-25 03:49:42 -0500

    Create 33 Merge sorted array.java

---
+++ 10 Sorting/33 Merge sorted array.java
@@ -0,0 +1,11 @@
+//O(n) time and O(1) space
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=m-1,j=n-1,k=m+n-1;
+        while(j>=0 && i>=0){
+            nums1[k--] = (nums1[i]>nums2[j])?nums1[i--]:nums2[j--];
+        }
+        while(j>=0)
+            nums1[k--] = nums2[j--];
+    }
+}
