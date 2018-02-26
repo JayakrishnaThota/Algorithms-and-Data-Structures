9f7ea23901003c880bd242f165ea08150961bdda
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2018-02-26 00:21:42 -0500

    Update kthlargestelementinanarray.java

--- 12 Selection Algorithms/kthlargestelementinanarray.java
+++ 12 Selection Algorithms/kthlargestelementinanarray.java
@@ -1,26 +1,49 @@
+//O(nlogk) and O(k)
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         int n = nums.length;
-        int p = quickSelect(nums,0,n-1,n-k+1);
-        return nums[p];
-    }
-    public int quickSelect(int[] nums, int low, int high, int k){
-        int i = low, j = high, pivot = nums[high];
-        while(i<j){
-            if(nums[i++]>pivot)
-                swap(nums,--i,--j);
+        k = n-k;
+        int low = 0, high = nums.length-1;
+        while(low<high){
+            int pivot = partition(nums, low, high);
+            if(pivot==k) break;
+            else if(pivot<k) low = pivot+1;
+            else high = pivot-1;
         }
-        swap(nums,i,high);
-        int m = i-low+1;
-        if(m==k) return i;
-        else if(m>k) return quickSelect(nums,low,i-1,k);
-        else return quickSelect(nums,i+1,high,k-m);
+        return nums[k];
     }
-                           
+   public int partition(int arr[], int low, int high)
+    {
+        int pivot = arr[high];
+        int i = (low-1); // index of smaller element
+        for (int j=low; j<high; j++)
+        {
+            // If current element is smaller than or
+            // equal to pivot
+            if (arr[j] <= pivot)
+            {
+                i++;
+                swap(arr,i,j);
+            }
+        }
+        // swap arr[i+1] and arr[high] (or pivot)
+        swap(arr,i+1,high);
+        return i+1;
+    }
     
-  void swap(int[] a, int i, int j) {
-    int tmp = a[i];
-    a[i] = a[j];
-    a[j] = tmp;
-  }
+    
+
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    public void shuffle(int[] nums){
+        Random rand = new Random();
+        for(int i=1;i<nums.length;i++){
+            swap(nums,i,rand.nextInt(i+1));
+        }
+    }
+    
 }
