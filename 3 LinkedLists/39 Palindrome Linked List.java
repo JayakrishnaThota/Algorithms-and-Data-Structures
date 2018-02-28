85b7013679ee55b81847b031c8e56919f1a5a4cf
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2018-02-28 06:37:41 -0500

    Completed

--- 3 LinkedLists/39 Palindrome Linked List.java
+++ 3 LinkedLists/39 Palindrome Linked List.java
@@ -1,18 +1,36 @@
-//O(n) and O(1)
-class Solution {
-    public ListNode swapPairs(ListNode head) {
-        if(head==null || head.next==null) return head;
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode cur = dummy;
-        while(cur.next!=null&&cur.next.next!=null){
-            ListNode first = cur.next;
-            ListNode second = cur.next.next;
-            first.next = second.next;
-            cur.next = second;
-            second.next = first;
-            cur = first;
+//O(n) and O(1) 
+public class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head==null || head.next == null) return true;
+        ListNode fast = head, slow = head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return dummy.next;
+        if(fast!=null) slow = slow.next;
+        
+        slow = reverse(slow);
+        fast = head;
+        while(slow!=null)
+        {
+            if(slow.val!=fast.val) return false;
+            slow=slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+    public ListNode reverse(ListNode head)
+    {
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr!=null)
+        {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
 }
